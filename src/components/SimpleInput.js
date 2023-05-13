cf2dd293ba73e20e27ea1d446666ba3367194ac5
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-05-13 21:35:43 +0430

    Refactoring & Deriving States

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,53 +1,35 @@
-import { useEffect, useRef, useState } from "react";
-import '../index.css'
+import {   useState } from "react";
+import "../index.css";
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  useEffect(() => {
-    if(enteredNameIsValid){
-      console.log('Name input is valid!');
-    }
-  }, [enteredNameIsValid])
+  const enteredNameIsValid = enteredName.trim() !== "";
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
-
-  const nameInputBlurHandler = event => {
-     setEnteredNameTouched(true);
-
-     if (enteredName.trim() === "") {
-       setEnteredNameIsValid(false);
-       return;
-     }
-  }
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+  };
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
+    setEnteredNameTouched(true);
 
-    setEnteredNameTouched(true)
-
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
 
-    setEnteredNameIsValid(true);
-    console.log(enteredName);
-    const enteredValue = nameInputRef.current.value;
     console.log(enteredName);
 
     setEnteredName("");
+    setEnteredNameTouched(false);
   };
-
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
-
 
   const nameInputClasses = nameInputIsInvalid
     ? "form-control invalid"
@@ -58,7 +40,6 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           type="text"
           id="name"
           onBlur={nameInputBlurHandler}
